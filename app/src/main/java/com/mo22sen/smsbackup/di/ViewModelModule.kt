ea6e260354e0275fb803e9cc8dcb8831ac7998ce
mo22sen <mohsenghafarpoor93@gmail.com>
2020-03-27 03:37:36 +0430

    di koin

--- app/src/main/java/com/mo22sen/smsbackup/di/ViewModelModule.kt
+++ app/src/main/java/com/mo22sen/smsbackup/di/ViewModelModule.kt
@@ -1,6 +1,11 @@
 package com.mo22sen.smsbackup.di
 
+import com.mo22sen.smsbackup.ui.MainViewModel
+import com.mo22sen.smsbackup.ui.home.HomeViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
+    viewModel { MainViewModel() }
+    viewModel { HomeViewModel() }
 }
