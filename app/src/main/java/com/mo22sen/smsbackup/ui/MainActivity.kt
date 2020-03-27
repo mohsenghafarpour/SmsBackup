a3cbac048d83465fdac7964d5707a78e3053d0d5
mo22sen <mohsenghafarpoor93@gmail.com>
2020-03-27 04:45:08 +0430

    checking permission

--- app/src/main/java/com/mo22sen/smsbackup/ui/MainActivity.kt
+++ app/src/main/java/com/mo22sen/smsbackup/ui/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.mo22sen.smsbackup.ui
 
+import android.Manifest.permission.READ_SMS
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.mo22sen.smsbackup.R
 import com.mo22sen.smsbackup.base.BaseActivity
 import com.mo22sen.smsbackup.databinding.ActivityMainBinding
+import com.mo22sen.smsbackup.utils.ktx.logD
 import org.koin.androidx.viewmodel.ext.android.viewModel
+
+const val SMS_PERMISSION = 1
 
 class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>() {
 
@@ -14,6 +21,19 @@ class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>() {
     override val navigationId: Int = R.id.main_navigation
 
     override fun configEvents() {
+
+        if (ContextCompat.checkSelfPermission(
+                this@MainActivity, READ_SMS
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            logD("permission is ok!!!")
+        } else {
+            ActivityCompat.requestPermissions(
+                this@MainActivity,
+                arrayOf(READ_SMS), SMS_PERMISSION
+            )
+        }
+
     }
 
     override fun bindObservables() {
